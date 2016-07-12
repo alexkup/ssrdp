f54c0ae13ecb928c6c313965c17fc5204be8399b
alexkup <alexkup@mail.ru>
2016-07-12 13:28:19 +0300

    Update tables.sql

--- db/tables.sql
+++ db/tables.sql
@@ -9,3 +9,12 @@ create table authdata (
 	password	varchar(255) not null,
 	domain	varchar(255),
 	caption varchar(255) not null);
+
+create table history(
+	id int primary key default 'autoincrement',
+	name varchar(255) not null,
+	authdata_id int not null references authdata(id) default '-1',
+	authusr varchar(255),
+	authpwd varchar(255),
+	caption varchar(255) not null,
+	ts datetime default (datetime('now','localtime')));	
