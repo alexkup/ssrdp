fcb14974f7fce6a559a88769094adf85697a4402
alexkup <alexkup@mail.ru>
2016-07-21 13:05:03 +0300

    Update tables.sql

--- db/tables.sql
+++ db/tables.sql
@@ -1,23 +1,29 @@
 create table hosts(
-	id int primary key autoincrement,
+	id integer primary key autoincrement,
 	name varchar(255) not null,
 	caption varchar(255) not null);
 	
 create table authdata (
-	id int primary key autoincrement,
+	id integer primary key autoincrement,
 	username	varchar(255) not null,
 	password	varchar(255) not null,
 	domain	varchar(255),
 	caption varchar(255) not null);
 
 create table history(
-	id int primary key autoincrement,
+	id integer primary key autoincrement,
 	name varchar(255) not null,
 	authdata_id int not null references authdata(id) default '-1',
 	authusr varchar(255),
 	authpwd varchar(255),
 	caption varchar(255) not null,
 	ts datetime default (datetime('now','localtime')));
+	
+create table rdpdata(
+	id integer primary key autoincrement,
+	hostdata_id int not null references hosts(id),
+	authdata_id int not null references authdata(id) default '-1',
+	caption varchar(255) not null unique);
 	
 insert into authdata(id,username,password,caption)
 select -1,'-','-','other auth data';
